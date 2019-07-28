570a8f842e18e6afb00c41aa21c5cc3f504cd493
web-design <gudlasiva123@gmail.com>
2019-07-28 08:51:14 +0530

    added

---
+++ html/IPL Teams/java.js
@@ -0,0 +1,14 @@
+
+<html>
+    <body>
+<script>
+function mouseDown() {
+    document.getElementById("myP").style.color = "red";
+}
+
+function mouseUp() {
+    document.getElementById("myP").style.color = "green";
+}
+</script>
+    </body>
+</html>
